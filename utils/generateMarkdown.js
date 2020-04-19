ee8d62f2955b0a0be2c8884ae4c6b842b7e09a43
AstridSuhartono <astrid_sh@yahoo.com>
2020-04-19 21:16:17 +0930

    create a base for the markdown

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,48 @@
+function generateMarkdown(data) {
+  return `
+# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+${data.contents}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+## License
+[![MIT License](https://img.shields.io/apm/l/atomic-design-ui.svg?)](https://github.com/tterb/atomic-design-ui/blob/master/LICENSEs)
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://opensource.org/licenses/)
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](http://www.gnu.org/licenses/agpl-3.0)  
+
+## Contributing
+
+${data.contents}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+Please contact me for any questions related to the application through the email below:
+
+![GitHub profile picture](${data.picture})
+
+Github Email: <${data.email}>
+
+`;
+}
+
+module.exports = generateMarkdown;
